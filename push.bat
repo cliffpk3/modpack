5bf185aac3024c5043f8024ad9fae9c851441d32
cliffpk3 <carli.ricardo@gmail.com>
2024-07-17 10:18:53 -0300

    2024-17-07 10:18

--- push.bat
+++ push.bat
@@ -14,11 +14,11 @@ for /f "tokens=1-2 delims=: " %%a in ('time /t') do (
 set currentDateTime=%year%-%month%-%day% %hour%:%minute%
 
 echo Data e hora atuais: %currentDateTime%
-pause
+
 
 set "currentDir=%cd%"
 echo Diretório atual: %currentDir%
-pause
+
 
 echo Procurando por arquivos maiores que 50MB...
 (for /r %%I in (*) do (
@@ -34,64 +34,65 @@ echo Procurando por arquivos maiores que 50MB...
     )
 )) >nul
 echo Concluído a procura por arquivos grandes.
-pause
+
 
 powershell -command "(Get-Content .gitignore) -replace '\\', '/' | Set-Content .gitignore"
 if %errorlevel% neq 0 (
     echo Erro ao converter barras no .gitignore
-    pause
+    
     exit /b %errorlevel%
 )
 echo Barras convertidas no .gitignore.
-pause
+
 
 powershell -command "Get-Content .gitignore | Sort-Object | Get-Unique | Set-Content .gitignore"
 if %errorlevel% neq 0 (
     echo Erro ao ordenar o .gitignore
-    pause
+    
     exit /b %errorlevel%
 )
 echo Duplicatas removidas e .gitignore ordenado.
-pause
+
 
 git add .gitignore
 if %errorlevel% neq 0 (
     echo Erro ao adicionar .gitignore ao Git
-    pause
+    
     exit /b %errorlevel%
 )
 git commit -m "GitIgnore %currentDateTime%"
 if %errorlevel% neq 0 (
     echo Erro ao fazer commit do .gitignore
-    pause
+    
     exit /b %errorlevel%
 )
 git push
 if %errorlevel% neq 0 (
     echo Erro ao fazer push do .gitignore
-    pause
+    
     exit /b %errorlevel%
 )
 echo Commit e push do .gitignore concluídos.
-pause
+
 
 git add .
 if %errorlevel% neq 0 (
     echo Erro ao adicionar mudanças ao Git
-    pause
+    
     exit /b %errorlevel%
 )
 git commit -m "%currentDateTime%"
 if %errorlevel% neq 0 (
     echo Erro ao fazer commit das mudanças
-    pause
+    
     exit /b %errorlevel%
 )
 git push
 if %errorlevel% neq 0 (
     echo Erro ao fazer push das mudanças
-    pause
+    
     exit /b %errorlevel%
 )
 echo Commit e push das mudanças concluídos.
+
 pause
