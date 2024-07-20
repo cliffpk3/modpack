c9f3ba4459d5ca1bdffde80356f166c64b09564d
cliffpk3 <carli.ricardo@gmail.com>
2024-07-19 22:59:10 -0300

    Commit manual 2024.07.19

--- push.bat
+++ push.bat
@@ -39,7 +39,7 @@ echo Concluído a procura por arquivos grandes.
 powershell -command "(Get-Content .gitignore) -replace '\\', '/' | Set-Content .gitignore"
 if %errorlevel% neq 0 (
     echo Erro ao converter barras no .gitignore
-    
+    pause    
     exit /b %errorlevel%
 )
 echo Barras convertidas no .gitignore.
@@ -48,7 +48,7 @@ echo Barras convertidas no .gitignore.
 powershell -command "Get-Content .gitignore | Sort-Object | Get-Unique | Set-Content .gitignore"
 if %errorlevel% neq 0 (
     echo Erro ao ordenar o .gitignore
-    
+    pause    
     exit /b %errorlevel%
 )
 echo Duplicatas removidas e .gitignore ordenado.
@@ -57,19 +57,19 @@ echo Duplicatas removidas e .gitignore ordenado.
 git add .gitignore
 if %errorlevel% neq 0 (
     echo Erro ao adicionar .gitignore ao Git
-    
+    pause    
     exit /b %errorlevel%
 )
 git commit -m "GitIgnore %currentDateTime%"
 if %errorlevel% neq 0 (
     echo Erro ao fazer commit do .gitignore
-    
+    pause    
     exit /b %errorlevel%
 )
 git push
 if %errorlevel% neq 0 (
     echo Erro ao fazer push do .gitignore
-    
+    pause    
     exit /b %errorlevel%
 )
 echo Commit e push do .gitignore concluídos.
@@ -78,19 +78,19 @@ echo Commit e push do .gitignore concluídos.
 git add .
 if %errorlevel% neq 0 (
     echo Erro ao adicionar mudanças ao Git
-    
+    pause    
     exit /b %errorlevel%
 )
 git commit -m "%currentDateTime%"
 if %errorlevel% neq 0 (
     echo Erro ao fazer commit das mudanças
-    
+    pause    
     exit /b %errorlevel%
 )
-git push
+git push --force
 if %errorlevel% neq 0 (
     echo Erro ao fazer push das mudanças
-    
+    pause
     exit /b %errorlevel%
 )
 echo Commit e push das mudanças concluídos.
